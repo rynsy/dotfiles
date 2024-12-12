0af28873389d3653596aaa83016465a8e331aa85
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-12-12 00:00:00 -0500

    automated backup

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,4 +3,11 @@
 -- Add any additional keymaps here
 vim.keymap.set("x", "<leader>p", '"_dP"')
 vim.keymap.set("n", ";", ":")
-vim.keymap.set("n", "<F5>", ":CMakeRun<CR>")
+-- vim.keymap.set("n", "<F5>", ":CMakeRun<CR>")
+
+-- Move terminal toggle to CTRL+SHIFT+/
+-- Unmap the default terminal toggle
+-- vim.keymap.del({ "n", "t" }, "<C-/>")
+-- Map CTRL+/ for single line comment
+-- vim.keymap.set("n", "<C-/>", "gcc", { remap = true }) -- Normal mode
+-- vim.keymap.set("v", "<C-/>", "gc", { remap = true }) -- Visual mode
